a6a9c1fb811bbb91965f0a872f8e0b68b214bbe9
Juan Miguel Sánchez Begines <jumisanbeg@gmail.com>
2018-06-14 18:29:24 +0000

    Show recipe an profile image implementation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# Se ponen los metodos que se van a usar en las vistas
 module ApplicationHelper
+  
+  def gravatar_for(user,options={size: 80})
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    size = options[:size]
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.chefname, class: "img-circle")
+  end
+  
 end
